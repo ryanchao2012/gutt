bff8d8208a8fdb62aa285be651edc04b113f051d
ryanchao2012 <ryanchao2012@gmail.com>
2021-04-10 21:50:38 +0800

    add: meta info in pyproject.toml for publishing to pypi

--- setup.py
+++ setup.py
@@ -10,14 +10,24 @@ try:
 except ImportError:
     from distutils.core import setup
 
+
+import os.path
+
 readme = ''
+here = os.path.abspath(os.path.dirname(__file__))
+readme_path = os.path.join(here, 'README.rst')
+if os.path.exists(readme_path):
+    with open(readme_path, 'rb') as stream:
+        readme = stream.read().decode('utf8')
+
 
 setup(
     long_description=readme,
     name='testgen',
-    version='0.2.1',
+    version='0.2.2',
     description='Auto generate templates for unit tests based on source code',
     python_requires='==3.*,>=3.6.0',
+    project_urls={"repository": "https://github.com/ryanchao2012/testgen"},
     author='ryanchao2012',
     author_email='ryanchao2012@gmail.com',
     license='MIT',
