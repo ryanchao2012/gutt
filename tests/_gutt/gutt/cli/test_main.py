90cec35e28023a47f898f27e715d54ef0d0112bf
ryanchao2012 <ryanchao2012@gmail.com>
2021-07-18 03:56:37 +0800

    add: test cases

--- tests/_gutt/gutt/cli/test_main.py
+++ tests/_gutt/gutt/cli/test_main.py
@@ -20,3 +20,27 @@ def test_print_version():
     from gutt.cli.main import print_version
 
     assert print_version
+
+
+class TestInvalidModule:
+    @classmethod
+    def setup_class(cls):
+        from gutt.cli.main import InvalidModule
+
+        assert InvalidModule
+
+    @classmethod
+    def teardown_class(cls):
+        pass
+
+    def setup_method(self, method):
+        pass
+
+    def teardown_method(self, method):
+        pass
+
+
+def test_collect_items_from_module():
+    from gutt.cli.main import collect_items_from_module
+
+    assert collect_items_from_module
