1a0726b028e65de746ccdf86f231cdcb9d2d5dde
ryanchao2012 <ryanchao2012@gmail.com>
2022-03-06 17:35:55 +0800

    regenerate tests

---
+++ tests/_gutt/gutt/gutt/test_utils.py
@@ -0,0 +1,46 @@
+def test_load_module_by_name():
+    from gutt.utils import load_module_by_name
+
+    pass
+
+
+def test_blacking():
+    from gutt.utils import blacking
+
+    pass
+
+
+def test_isorting():
+    from gutt.utils import isorting
+
+    pass
+
+
+def test_expand_sys_path():
+    from gutt.utils import expand_sys_path
+
+    pass
+
+
+def test_makefile():
+    from gutt.utils import makefile
+
+    pass
+
+
+def test__writefile():
+    from gutt.utils import _writefile
+
+    pass
+
+
+def test_catch_module_from_sys():
+    from gutt.utils import catch_module_from_sys
+
+    pass
+
+
+def test_populate_init():
+    from gutt.utils import populate_init
+
+    pass
