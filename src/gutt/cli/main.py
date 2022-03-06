318f61133cb9ff543a0d15728db8a056799adec0
ryanchao2012 <ryanchao2012@gmail.com>
2022-03-06 17:39:59 +0800

    change: main cli

--- src/gutt/cli/main.py
+++ src/gutt/cli/main.py
@@ -1,13 +1,12 @@
 import os
 import re
 from collections import OrderedDict
-from pathlib import Path
 
 import click
 from asttrs import ClassDef, FunctionDef, Module
 from gutt.model import Code, ModuleIO
-from gutt.template import Template
-from gutt.utils import expand_sys_path, makefile
+from gutt.template import Template as T
+from gutt.utils import blacking, expand_sys_path, makefile, populate_init
 
 
 class InvalidModule(Exception):
@@ -57,35 +56,62 @@ def print_version(ctx, param, value):
     help='Output root directory for populating test files, default: "tests/_gutt".',
 )
 @click.option(
-    "--template",
+    "--template-class",
     "-t",
-    # default="tests/_gutt",
-    # help='Output root directory for populating test files, default: "tests/_gutt".',
+    default="gutt.template.Template",
+    help='Assign template class to generate test template, default: "gutt.template.Template".',
 )
-def main(ctx, modname, path, exclude, output, template):
+@click.option(
+    "--dryrun",
+    "-dr",
+    is_flag=True,
+    help="Run in dryrun mode.",
+)
+def main(ctx, modname, path, exclude, output, template_class, dryrun):
 
     with expand_sys_path(*path):
         module: ModuleIO = ModuleIO.from_name(modname, output)
+        Template = T.load(template_class)
 
         if module is None:
             raise InvalidModule(modname)
 
         for mod in module.submodules:
 
+            code_added = 0
+
+            if isinstance(exclude, str) and re.search(exclude, mod.name):
+                click.secho("ignoring module: ", nl=False, fg="bright_white")
+                click.secho(mod.name, fg="bright_black")
+                continue
+
             try:
                 src_mod = Module.from_file(mod.src)
 
             except Exception as error:
-                # TODO: warning
-                print("!!!", type(error), error, mod.src)
+                msg = f'{type(error)}: {error}. src: "{mod.src}"'
+                click.secho(msg, fg="bright_yellow")
                 continue
 
-            # TODO: exclude
-            src_codes = OrderedDict(
-                (f"{mod.name}.{el.name}", Code(module=mod, ast=el))
-                for el in src_mod.body
-                if isinstance(el, (FunctionDef, ClassDef))
-            )
+            src_codes = OrderedDict()
+
+            for el in src_mod.body:
+                if not isinstance(el, (FunctionDef, ClassDef)):
+                    continue
+
+                qname = f"{mod.name}.{el.name}"
+
+                if isinstance(exclude, str) and re.search(exclude, qname):
+                    click.secho("excluding: ", nl=False, fg="bright_white")
+                    click.secho(qname, fg="bright_black")
+
+                    continue
+
+                src_codes.update({qname: Code(module=mod, ast=el)})
+
+                click.echo("\033[K", nl=False)
+                click.secho("collecting: ", nl=False, fg="bright_white")
+                click.secho(f"{qname}", fg="bright_cyan")
 
             if len(src_codes) == 0:
                 continue
@@ -93,12 +119,15 @@ def main(ctx, modname, path, exclude, output, template):
             try:
                 test_mod = Module.from_file(mod.dst)
 
+                click.secho("loading: ", nl=False, fg="bright_white")
+                click.secho(f"{mod.dst}", fg="bright_green")
+
             except FileNotFoundError:
                 test_mod = Module()
 
             except Exception as error:
-                # TODO: warning
-                print("!!!", type(error), error, mod.dst)
+                msg = f'{type(error)}: {error}. dst: "{mod.dst}"'
+                click.secho(msg, fg="bright_yellow")
                 continue
 
             test_codes = OrderedDict()
@@ -119,20 +148,51 @@ def main(ctx, modname, path, exclude, output, template):
 
                 test_codes.update({key: Code(module=mod, ast=el)})
 
-            body = []
-
             for key, tcode in test_codes.items():
 
-                if key in src_codes:
+                if key not in src_codes:
+                    continue
 
-                    scode = src_codes.pop(key)
+                scode = src_codes.pop(key)
 
-                    if isinstance(scode, ClassDef):
-                        # TODO: check the differences
-                        # tcode = ...
-                        pass
+                if not isinstance(scode.ast, ClassDef):
+                    continue
 
-                body.append(tcode.ast)
+                test_pfx = Template.class_layout.method_layout.prefix
+                tmethods = OrderedDict()
+                for j, el in enumerate(tcode.ast.body):
+
+                    key = el.name if isinstance(el, FunctionDef) else f"#{j}"
+
+                    tmethods.update({key: el})
+
+                for el in scode.ast.body:
+                    # TODO: allow private methods?
+                    if (not isinstance(el, FunctionDef)) or el.name.startswith("__"):
+                        continue
+
+                    tname = f"{test_pfx}{el.name}"
+
+                    if tname not in tmethods:
+
+                        f = Template.class_layout.method_layout(
+                            Code(module=scode.module, ast=el)
+                        ).build()
+
+                        click.echo("\033[K", nl=False)
+                        click.secho(
+                            "adding method: ",
+                            nl=False,
+                            fg="bright_white",
+                        )
+                        click.secho(
+                            f"{scode.ast.name}:{el.name}",
+                            fg="bright_cyan",
+                        )
+
+                        tcode.ast.body.append(f)
+
+                        code_added += 1
 
             for key, scode in src_codes.items():
 
@@ -142,36 +202,37 @@ def main(ctx, modname, path, exclude, output, template):
                     else Template.class_layout
                 )
 
-                # TODO: append imp to output module body with defined template
-                body.append(Layout(scode).build())
+                what = "function" if isinstance(scode.ast, FunctionDef) else "class"
 
-            test_mod = Module(body=body)
+                obj = Layout(scode).build()
+                click.echo("\033[K", nl=False)
+                click.secho(f"adding {what}: ", nl=False, fg="bright_white")
+                click.secho(f"{obj.name}", fg="bright_cyan")
 
-            print("#", mod.dst)
-            # test_mod.show()
-            makefile(mod.dst)
-            test_mod.to_file(mod.dst)
+                test_mod.body.append(obj)
+                code_added += 1
 
-    output_root = Path(output)
-    paths = {output_root}
-    for path in output_root.glob("**/*.py"):
-        if path.name == "__init__.py":
-            continue
+            if code_added > 0:
+                test_mod = Module(body=test_mod.body)
+                try:
+                    source = blacking(test_mod.to_source())
+                except Exception as error:
+                    msg = f'{type(error).__name__}: {error}. src: "{mod.src}"'
+                    click.secho(msg, fg="bright_red")
+                    click.secho(test_mod.to_source(), fg="bright_yellow")
+                    continue
 
-        parent = path.parent
+                if dryrun:
+                    click.secho("(dryrun)", nl=False, fg="bright_yellow")
+                    click.secho(f" writing: {mod.dst}", fg="bright_black")
 
-        while parent > output_root:
+                else:
+                    makefile(mod.dst, source, overwrite=True)
 
-            if not parent.joinpath("__init__.py").exists():
-                paths.add(parent)
+                    click.secho("writing: ", nl=False, fg="bright_white")
+                    click.secho(f"{mod.dst}", fg="bright_green")
 
-            parent = parent.parent
+            else:
+                click.secho("all templates populated, skip.", fg="bright_black")
 
-    for p in paths:
-        makefile(os.path.join(str(p), "__init__.py"))
-
-
-def _echo_collect(name: str, i: int = 1):
-    click.echo("\033[K", nl=False)
-    click.secho(f"collecting {i} item: ", nl=False, fg="bright_white")
-    click.secho(f"{name}\r", nl=False, fg="bright_cyan")
+    populate_init(output)
